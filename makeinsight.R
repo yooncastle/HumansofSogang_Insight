399b7cd863197f61b4b23afc16087c00a819ed19
Haesung Kim <HaesungKim@Haesungui-MacBook-Air.local>
2017-08-10 16:50:39 -0700

    changed merged things

--- makeinsight.R
+++ makeinsight.R
@@ -1,12 +1,12 @@
 #Before reading this file, you need to go on Tools -> Global Options -> Code -> Savings -> and set the Default text encoding to UTF-8
 
 library(readr)
-postings <- read_csv("Postings2.csv")
-View(postings)
-summary(postings)
-
 library(dplyr)
 library(ggplot2)
+
+#Read Postings
+postings <- read_csv("Postings2.csv")
+View(postings)
 
 reached = as.numeric(postings$'전체 기간 내 게시물 총 도달수')
 postings %>% ggplot(aes(x=reached)) + geom_histogram()
@@ -17,10 +17,10 @@ postings %>% ggplot(aes(x=date)) +geom_histogram()
 
 plot(date, reached, cex=1)
 
+#Read Pageinfo
 pageinfo <- read_csv("pageinfo.csv")
 
 View(pageinfo)
-summary(pageinfo)
 
 reached = as.numeric(pageinfo$'전체 기간 총 좋아요 수')
 date = as.Date(pageinfo$'날짜',"%Y.%M.%D")
