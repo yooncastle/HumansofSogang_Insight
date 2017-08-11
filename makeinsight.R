33c96231f15ea57bb6b424c3c69593bceb51194b
Haesung Kim <HaesungKim@Haesungui-MacBook-Air.local>
2017-08-10 17:07:38 -0700

    added codes

--- makeinsight.R
+++ makeinsight.R
@@ -1,8 +1,8 @@
 #Before reading this file, you need to go on Tools -> Global Options -> Code -> Savings -> and set the Default text encoding to UTF-8
-
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 #Read Postings
 postings <- read_csv("Postings2.csv")
@@ -17,13 +17,31 @@ postings %>% ggplot(aes(x=date)) +geom_histogram()
 
 plot(date, reached, cex=1)
 
-#Read Pageinfo
 pageinfo <- read_csv("pageinfo.csv")
+
+#pageinfo <- pageinfo %>% select(-1)
+#column 삭제하기
 
 View(pageinfo)
 
 reached = as.numeric(pageinfo$'전체 기간 총 좋아요 수')
-date = as.Date(pageinfo$'날짜',"%Y.%M.%D")
-#왜 date가 안읽힐까요..?!
+date <- lubridate::parse_date_time(pageinfo$'날짜',"%Y.%m.%d")
 
-pageinfo %>% ggplot(aes(x=reached)) + geom_histogram()
+pageinfo %>% 
+  ggplot(aes(x=reached)) + geom_histogram()
+
+pageinfo %>%
+  ggplot(aes(reached)) + geom_histogram() + 
+  geom_hline(yintercept=30, col='red')
+
+pageinfo %>%
+  ggplot(aes(date, reached)) + geom_histogram() + 
+  geom_hline(yintercept=30, col='red')
+#왜안될까요?
+
+plot(date, reached, cex=1)
+
+
+
+
+
